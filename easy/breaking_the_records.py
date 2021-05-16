bf22d5c7af0deb428fceb29123b7cd354b5019fc
Seth Obey <47947793+sethobey@users.noreply.github.com>
2021-05-16 00:56:51 -0500

    Create breaking_the_records.py

---
+++ easy/breaking_the_records.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    min = max = scores[0]
+    result = [0, 0]
+    for score in scores[1:]:
+        if score > max:
+            max = score
+            result[0] += 1
+        elif score < min:
+            min = score
+            result[1] += 1 
+    return result
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
